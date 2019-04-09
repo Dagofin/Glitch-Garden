7bf26d343e3fcc4a3ea71effdcc849041fec4bd6
dagofin <gavin.j.taggart@gmail.com>
2019-04-09 13:15:17 -0500

    Added a defender and implemented defender shooting

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject projectileSpawn;
+
+    public void Fire()
+    {
+
+        Instantiate(projectile, projectileSpawn.transform.position, Quaternion.identity);
+
+
+    }
+
+}
